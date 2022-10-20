8ed117dada4030c8921ad9f25f917a6e98fab216
YamiDigangi <112526959+YamiDigangi@users.noreply.github.com>
2022-10-20 09:48:18 -0300

    Initial commit

---
+++ su_boleteria (1).sql
@@ -0,0 +1,210 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 18-10-2022 a las 16:07:06
+-- Versión del servidor: 10.4.24-MariaDB
+-- Versión de PHP: 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `su_boleteria`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `butaca`
+--
+
+CREATE TABLE `butaca` (
+  `idButaca` int(11) NOT NULL,
+  `idSala` int(11) NOT NULL,
+  `fila` int(10) NOT NULL,
+  `columna` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `cliente`
+--
+
+CREATE TABLE `cliente` (
+  `idCliente` int(11) NOT NULL,
+  `dni` int(11) NOT NULL,
+  `nombre` varchar(25) NOT NULL,
+  `apellido` varchar(25) NOT NULL,
+  `estado` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `pelicula`
+--
+
+CREATE TABLE `pelicula` (
+  `idPelicula` int(11) NOT NULL,
+  `nombrePeli` varchar(50) NOT NULL,
+  `estadoPeli` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `proyeccion`
+--
+
+CREATE TABLE `proyeccion` (
+  `idProyeccion` int(11) NOT NULL,
+  `idSala` int(11) NOT NULL,
+  `idPelicula` int(11) NOT NULL,
+  `inicioPro` date NOT NULL,
+  `finPro` date NOT NULL,
+  `estadoPro` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `sala`
+--
+
+CREATE TABLE `sala` (
+  `idSala` int(11) NOT NULL,
+  `ubicacion` varchar(35) NOT NULL,
+  `localidad` varchar(50) NOT NULL,
+  `estadoSala` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `tiket`
+--
+
+CREATE TABLE `tiket` (
+  `idTiket` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `idProyeccion` int(11) NOT NULL,
+  `idButaca` int(11) NOT NULL,
+  `fechaCompra` date NOT NULL,
+  `monto` double NOT NULL,
+  `formaPago` varchar(50) NOT NULL,
+  `estadoTiket` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `butaca`
+--
+ALTER TABLE `butaca`
+  ADD PRIMARY KEY (`idButaca`),
+  ADD KEY `idSala` (`idSala`);
+
+--
+-- Indices de la tabla `cliente`
+--
+ALTER TABLE `cliente`
+  ADD PRIMARY KEY (`idCliente`);
+
+--
+-- Indices de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  ADD PRIMARY KEY (`idPelicula`);
+
+--
+-- Indices de la tabla `proyeccion`
+--
+ALTER TABLE `proyeccion`
+  ADD PRIMARY KEY (`idProyeccion`),
+  ADD KEY `idPelicula` (`idPelicula`),
+  ADD KEY `idSala` (`idSala`);
+
+--
+-- Indices de la tabla `sala`
+--
+ALTER TABLE `sala`
+  ADD PRIMARY KEY (`idSala`);
+
+--
+-- Indices de la tabla `tiket`
+--
+ALTER TABLE `tiket`
+  ADD PRIMARY KEY (`idTiket`),
+  ADD KEY `idCliente` (`idCliente`),
+  ADD KEY `idButaca` (`idButaca`),
+  ADD KEY `idProyeccion` (`idProyeccion`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `pelicula`
+--
+ALTER TABLE `pelicula`
+  MODIFY `idPelicula` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `proyeccion`
+--
+ALTER TABLE `proyeccion`
+  MODIFY `idProyeccion` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `sala`
+--
+ALTER TABLE `sala`
+  MODIFY `idSala` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT de la tabla `tiket`
+--
+ALTER TABLE `tiket`
+  MODIFY `idTiket` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `butaca`
+--
+ALTER TABLE `butaca`
+  ADD CONSTRAINT `butaca_ibfk_1` FOREIGN KEY (`idSala`) REFERENCES `sala` (`idSala`);
+
+--
+-- Filtros para la tabla `proyeccion`
+--
+ALTER TABLE `proyeccion`
+  ADD CONSTRAINT `proyeccion_ibfk_1` FOREIGN KEY (`idPelicula`) REFERENCES `pelicula` (`idPelicula`),
+  ADD CONSTRAINT `proyeccion_ibfk_2` FOREIGN KEY (`idSala`) REFERENCES `sala` (`idSala`);
+
+--
+-- Filtros para la tabla `tiket`
+--
+ALTER TABLE `tiket`
+  ADD CONSTRAINT `tiket_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `cliente` (`idCliente`),
+  ADD CONSTRAINT `tiket_ibfk_2` FOREIGN KEY (`idButaca`) REFERENCES `butaca` (`idButaca`),
+  ADD CONSTRAINT `tiket_ibfk_3` FOREIGN KEY (`idProyeccion`) REFERENCES `proyeccion` (`idProyeccion`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
